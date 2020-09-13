03e6114295509e1ae451b16d873dc2d21b7f9ce2
bamgasi <bamgasi@gmail.com>
2020-09-13 15:36:45 +0900

    뭔가 많이 바뀌었다

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,84 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="site.title" />
+      <v-spacer />
+
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" width="400">
+
+      <site-menu :items="site.menu"></site-menu>
+
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view />
+    </v-main>
+    <site-footer :footer="site.footer" />
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SiteTitle from '@/views/site/Title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    SiteTitle, SiteFooter, SiteMenu
+  },
+  data: () => ({
+    drawer: false,
+    site: {
+      title: 'Site Title',
+      footer: 'Site Footer',
+      menu: [{
+        title: 'home',
+        icon: 'mdi-home',
+        subItems: [
+          {
+            title: 'Dashboard',
+            to: '/'
+          },
+          {
+            title: 'About',
+            to: '/about'
+          }
+        ],
+        to: '/'
+      },
+      {
+        title: 'About',
+        icon: 'mdi-help-box',
+        subItems: [
+          {
+            title: 'XXX',
+            to: '/xxx'
+          }
+        ],
+        to: '/about',
+        active: true
+      }]
+    }
+  }),
+  created () {
+    this.subscribe()
+  },
+  methods: {
+    subscribe () {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
+        const v = sn.val()
+        if (!v) {
+          this.$firebase.database().ref().child('site').set(this.site)
+        } else {
+          this.site = v
+        }
+      }, (e) => {
+        console.log(e.message)
+      })
+    }
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ src/views/site/menu.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="title">
+          Application
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          subtext
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-group
+        v-for="(item, index) in items"
+        :key="index"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn @click="openDialogItem(index)" icon><v-icon>mdi-pencil</v-icon></v-btn>
+          </v-list-item-action>
+        </template>
+
+        <v-list-item
+          v-for="(subItem, j) in item.subItems"
+          :key="j"
+          :to="subItem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+        <v-list-item-icon><v-icon>mdi-plus</v-icon></v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>서브추가하기</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      </v-list-group>
+
+      <v-list-item @click="openDialogItem(-1)">
+        <v-list-item-icon><v-icon>mdi-plus</v-icon></v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>추가하기</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-dialog v-model="dialogItem" max-width="400">
+      <v-card>
+        <v-card-title>
+          수정하기
+          <v-spacer></v-spacer>
+          <v-btn icon @click="saveItem"><v-icon>mdi-content-save</v-icon></v-btn>
+          <v-btn icon @click="dialogItem = false"><v-icon>mdi-close</v-icon></v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="formItem.title" @keypress.enter="saveItem"></v-text-field>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      dialogItem: false,
+      dialogSubItem: false,
+      formItem: {
+        icon: '',
+        title: ''
+      },
+      selectedItemIndex: -1
+    }
+  },
+  methods: {
+    openDialogItem (index) {
+      this.dialogItem = true
+      this.selectedItemIndex = index
+      if (index < 0) {
+        this.formItem.title = ''
+      } else {
+        this.formItem.title = this.items[index].title
+      }
+    },
+    saveItem () {
+      if (this.selectedItemIndex < 0) {
+        this.items.push(this.formItem)
+      } else {
+        this.items[this.selectedItemIndex] = this.formItem
+      }
+      this.save()
+      this.dialogItem = false
+    },
+    async save () {
+      try {
+        await this.$firebase.database().ref().child('site').update({ menu: this.items })
+      } finally {
+        this.dialog = false
+      }
+    }
+  }
+}
+</script>
